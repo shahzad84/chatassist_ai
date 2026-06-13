e4f8956484274c87531662b36d6f93ca366246dc
shahzad84 <shahzad20022002@gmail.com>
2026-06-13 19:06:47 +0530

    added database constraints

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -2,6 +2,7 @@ import Database from "better-sqlite3";
 import type { Database as BetterSqliteDatabase } from "better-sqlite3";
 
 const db: BetterSqliteDatabase = new Database("chat.db");
+db.pragma("foreign_keys = ON");
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS conversations (
@@ -12,13 +13,22 @@ CREATE TABLE IF NOT EXISTS conversations (
 CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   conversation_id TEXT NOT NULL,
-  sender TEXT NOT NULL,
+
+  sender TEXT NOT NULL
+  CHECK(sender IN ('user', 'ai')),
+
   text TEXT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
   FOREIGN KEY (conversation_id)
   REFERENCES conversations(id)
+  ON DELETE CASCADE
 );
+
+  CREATE INDEX IF NOT EXISTS
+  idx_messages_conversation_id
+  ON messages(conversation_id);
+
 `);
 
 export default db;
